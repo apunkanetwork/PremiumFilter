e0e673e64a80f965ca87b38abfdee6344e45b26e
Enoxside <105333651+apunkanetwork@users.noreply.github.com>
2023-04-04 13:28:22 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -4,4 +4,4 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("GreyMatters")
+    return web.json_response("Oxyver")
